441f41517c16b0eff0d1816849a24a25c2dba891
saravananpushparaj <saravananpushparaj@gmail.com>
2017-05-04 11:54:48 +0530

    V2.0

--- src/test/java/Day1/Testcase1.java
+++ src/test/java/Day1/Testcase1.java
@@ -5,7 +5,8 @@ public class Testcase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hi Jan batch");
+		//System.out.println("Hi Jan batch");
+		System.out.println("Hello Welcome");
 
 	}
 
